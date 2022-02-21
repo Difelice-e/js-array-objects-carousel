2c2a5b1c896079e2c1eae4c6c14d75e8b4bcecb0
Difelice-e <difelice.emanuele@gmail.com>
2022-02-21 23:48:23 +0100

    riordinato js

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-// const items = [
-//     'img/01.jpg',
-//     'img/02.jpg',
-//     'img/03.jpg',
-//     'img/04.jpg',
-//     'img/05.jpg'
-// ];
-
-// const title = [
-//     'Svezia',
-//     'Svizzera',
-//     'Gran Bretagna',
-//     'Germania',
-//     'Paradise'
-// ];
-
-// const text = [
-//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-//     'Lorem ipsum',
-//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-//     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-//     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-// ];
-
-
+// dichiarazione array    
 const slideItem = [
     {
         item: 'img/01.jpg',
@@ -60,43 +36,11 @@ const slideItem = [
     }
 ]
 
-console.log(slideItem)
 
+// creazione della parte principale del carosello
 let currentIndex = 0;
 let divElement;
 const sliderWrapper = document.querySelector('.slider-wrapper');
-
-// for (i = 0; i < items.length; i++) {
-//     divElement = document.createElement('div');
-//     console.log(divElement);
-//     const divInfo = document.createElement('div');
-//     console.log(divInfo);
-//     const imgElement = document.createElement('img');
-//     imgElement.src = `${items[i]}`;
-//     console.log(imgElement);
-//     const divTitle = `<h1>${title[i]}</h1>`;
-//     console.log(divTitle)
-//     const divText = `<p>${text[i]}</p>`;
-//     console.log(divText);
-    
-//     console.log(sliderWrapper);
-
-//     if (i == 0) {
-//         divElement.classList.add("active");
-//         currentIndex = 0;
-//     }
-    
-//     sliderWrapper.append(divElement);
-//     divInfo.innerHTML += divTitle + divText;
-//     divElement.append(imgElement, divInfo);
-    
-
-//     divElement.classList.add("item");
-//     divInfo.classList.add("item-info");
-
-//     // console.log(divInfo, divElement)
-    
-// };
 
 slideItem.forEach(({item, title, text, active}, index) => {
     divElement = document.createElement('div');
@@ -113,11 +57,6 @@ slideItem.forEach(({item, title, text, active}, index) => {
     
     console.log(sliderWrapper);
 
-    // if (index == 0) {
-    //     divElement.classList.add("active");
-    //     currentIndex = 0;
-    // }
-
     if (active) {
         divElement.classList.add('active');
     }
@@ -132,6 +71,7 @@ slideItem.forEach(({item, title, text, active}, index) => {
 })
 
 
+// creazione zona dei controlli   
 let imgThumbElement;
 let prevControl;
 let nextControl;
@@ -142,11 +82,6 @@ slideItem.forEach(({item, active}, index) => {
     
     imgThumbElement = document.createElement('img');
     imgThumbElement.src = `${item}`;
-
-    // if (index == 0) {
-    //     imgThumbElement.classList.add("active");
-    //     currentIndex = 0;
-    // }
 
     if (active) {
         imgThumbElement.classList.add('active');
@@ -167,40 +102,7 @@ prevControl = document.createElement('span');
 
 controlWrapper.append(prevControl, nextControl);
 
-
-const next = document.querySelector('.next')
-
-let divElements = document.querySelectorAll('.slider-wrapper > div'); //creazione array
-let imgThumbElements = document.querySelectorAll('.control-wrapper > img');
-
-next.addEventListener("click", function() {
-    console.log("next!")
-    
-    // divElements[currentIndex].classList.remove("active");
-    // imgThumbElements[currentIndex].classList.remove("active");
-
-    slideItem[currentIndex].active = false;
-    console.log(slideItem[currentIndex].active)
-
-    if ( currentIndex < slideItem.length -1 ) {
-        currentIndex++
-    
-        // divElements[currentIndex].classList.add("active");
-        // imgThumbElements[currentIndex].classList.add("active");
-
-        slideItem[currentIndex].active = true;
-    } else {
-        currentIndex = 0;
-    
-        // divElements[currentIndex].classList.add("active");
-        // imgThumbElements[currentIndex].classList.add("active");
-
-        slideItem[currentIndex].active = true;
-    }
-    
-    controlActive();
-});
-
+// dichiarazione funzione di controllo classe active 
 function controlActive() {
     slideItem.forEach( (element, index, array) => {
         if (element.active) {
@@ -213,28 +115,46 @@ function controlActive() {
     } )
 }
 
-const prev = document.querySelector('.prev')
+// controllo next
+const next = document.querySelector('.next')
 
+let divElements = document.querySelectorAll('.slider-wrapper > div'); //creazione array
+let imgThumbElements = document.querySelectorAll('.control-wrapper > img');
+
+next.addEventListener("click", function() {
+    console.log("next!")
+
+    slideItem[currentIndex].active = false;
+    console.log(slideItem[currentIndex].active)
+
+    if ( currentIndex < slideItem.length -1 ) {
+        currentIndex++
+
+        slideItem[currentIndex].active = true;
+    } else {
+        currentIndex = 0;
+
+        slideItem[currentIndex].active = true;
+    }
+    
+    controlActive();
+});
+
+
+// controllo prev
+const prev = document.querySelector('.prev')
 
 prev.addEventListener("click", function() {
     console.log("prev!")
-    // divElements[currentIndex].classList.remove("active");
-    // imgThumbElements[currentIndex].classList.remove("active");
 
     slideItem[currentIndex].active = false;
     
     if ( currentIndex > 0 ) {
         currentIndex--
-    
-        // divElements[currentIndex].classList.add("active");
-        // imgThumbElements[currentIndex].classList.add("active");
 
         slideItem[currentIndex].active = true;
     } else {
         currentIndex = slideItem.length - 1;
-    
-        // divElements[currentIndex].classList.add("active");
-        // imgThumbElements[currentIndex].classList.add("active");
 
         slideItem[currentIndex].active = true;
     }
